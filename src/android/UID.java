d06e3e47b10e386435d5375a771e90445fca1f75
Muhammad Awais <awais.sds@gmail.com>
2020-10-20 12:16:52 +0500

    UID.java for latest android

--- src/android/UID.java
+++ src/android/UID.java
@@ -6,6 +6,7 @@
 package org.hygieiasoft.cordova.uid;
 
 import android.content.Context;
+import android.os.Build;
 import android.telephony.TelephonyManager;
 import android.provider.Settings;
 import android.net.wifi.WifiInfo;
@@ -21,118 +22,132 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class UID extends CordovaPlugin {
-	public static String uuid; // Device UUID
-	public static String imei; // Device IMEI
-	public static String imsi; // Device IMSI
-	public static String iccid; // Sim IMSI
-	public static String mac; // MAC address
+    public static String uuid; // Device UUID
+    public static String imei; // Device IMEI
+    public static String imsi; // Device IMSI
+    public static String iccid; // Sim IMSI
+    public static String mac; // MAC address
 
-	/**
-	 * Constructor.
-	 */
-	public UID() {
-	}
+    /**
+     * Constructor.
+     */
+    public UID() {
+    }
 
-	/**
-	 * Sets the context of the Command. This can then be used to do things like
-	 * get file paths associated with the Activity.
-	 *
-	 * @param cordova The context of the main Activity.
-	 * @param webView The CordovaWebView Cordova is running in.
-	 */
-	@Override
-	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-		super.initialize(cordova, webView);
-		Context context = cordova.getActivity().getApplicationContext();
-		UID.uuid = getUuid(context);
-		UID.imei = getImei(context);
-		UID.imsi = getImsi(context);
-		UID.iccid = getIccid(context);
-		UID.mac = getMac(context);
-	}
+    /**
+     * Sets the context of the Command. This can then be used to do things like
+     * get file paths associated with the Activity.
+     *
+     * @param cordova The context of the main Activity.
+     * @param webView The CordovaWebView Cordova is running in.
+     */
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        Context context = cordova.getActivity().getApplicationContext();
+        UID.uuid = getUuid(context);
+        UID.imei = getImei(context);
+        //UID.imsi = getImsi(context);
+        //UID.iccid = getIccid(context);
+        UID.mac = getMac(context);
+    }
 
-	/**
-	 * Executes the request and returns PluginResult.
-	 *
-	 * @param action            The action to execute.
-	 * @param args              JSONArry of arguments for the plugin.
-	 * @param callbackContext   The callback id used when calling back into JavaScript.
-	 * @return                  True if the action was valid, false if not.
-	 */
-	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		if (action.equals("getUID")) {
-			JSONObject r = new JSONObject();
-			r.put("UUID", UID.uuid);
-			r.put("IMEI", UID.imei);
-			r.put("IMSI", UID.imsi);
-			r.put("ICCID", UID.iccid);
-			r.put("MAC", UID.mac);
-			callbackContext.success(r);
-		} else {
-			return false;
-		}
-		return true;
-	}
+    /**
+     * Executes the request and returns PluginResult.
+     *
+     * @param action          The action to execute.
+     * @param args            JSONArry of arguments for the plugin.
+     * @param callbackContext The callback id used when calling back into JavaScript.
+     * @return True if the action was valid, false if not.
+     */
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("getUID")) {
+            JSONObject r = new JSONObject();
+            r.put("UUID", UID.uuid);
+            r.put("IMEI", UID.imei);
+            r.put("IMSI", UID.imsi);
+            r.put("ICCID", UID.iccid);
+            r.put("MAC", UID.mac);
+            callbackContext.success(r);
+        } else {
+            return false;
+        }
+        return true;
+    }
 
-	/**
-	 * Get the device's Universally Unique Identifier (UUID).
-	 *
-	 * @param context The context of the main Activity.
-	 * @return
-	 */
-	public String getUuid(Context context) {
-		String uuid = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
-		return uuid;
-	}
+    /**
+     * Get the device's Universally Unique Identifier (UUID).
+     *
+     * @param context The context of the main Activity.
+     * @return
+     */
+    public String getUuid(Context context) {
+        String uuid = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+        return uuid;
+    }
 
-	/**
-	 * Get the device's International Mobile Station Equipment Identity (IMEI).
-	 *
-	 * @param context The context of the main Activity.
-	 * @return
-	 */
-	public String getImei(Context context) {
-		final TelephonyManager mTelephony = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-		String imei = mTelephony.getDeviceId();
-		return imei;
-	}
+    /**
+     * Get the device's International Mobile Station Equipment Identity (IMEI).
+     *
+     * @param context The context of the main Activity.
+     * @return
+     */
+    public String getImei(Context context) {
+        String deviceId;
 
-	/**
-	 * Get the device's International mobile Subscriber Identity (IMSI).
-	 *
-	 * @param context The context of the main Activity.
-	 * @return
-	 */
-	public String getImsi(Context context) {
-		final TelephonyManager mTelephony = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-		String imsi = mTelephony.getSubscriberId();
-		return imsi;
-	}
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            deviceId = Settings.Secure.getString(
+                    context.getContentResolver(),
+                    Settings.Secure.ANDROID_ID);
+        } else {
+            final TelephonyManager mTelephony = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+            if (mTelephony.getDeviceId() != null) {
+                deviceId = mTelephony.getDeviceId();
+            } else {
+                deviceId = Settings.Secure.getString(
+                        context.getContentResolver(),
+                        Settings.Secure.ANDROID_ID);
+            }
+        }
+        return deviceId;
+    }
 
-	/**
-	 * Get the sim's Integrated Circuit Card Identifier (ICCID).
-	 *
-	 * @param context The context of the main Activity.
-	 * @return
-	 */
-	public String getIccid(Context context) {
-		final TelephonyManager mTelephony = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
-		String iccid = mTelephony.getSimSerialNumber();
-		return iccid;
-	}
+    /**
+     * Get the device's International mobile Subscriber Identity (IMSI).
+     *
+     * @param context The context of the main Activity.
+     * @return
+     */
+    public String getImsi(Context context) {
+        final TelephonyManager mTelephony = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        String imsi = mTelephony.getSubscriberId();
+        return imsi;
+    }
 
-	/**
-	 * Get the Media Access Control address (MAC).
-	 *
-	 * @param context The context of the main Activity.
-	 * @return
-	 */
-	public String getMac(Context context) {
-		final WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-		final WifiInfo wInfo = wifiManager.getConnectionInfo();
-		String mac = wInfo.getMacAddress();
-		return mac;
-	}
+    /**
+     * Get the sim's Integrated Circuit Card Identifier (ICCID).
+     *
+     * @param context The context of the main Activity.
+     * @return
+     */
+    public String getIccid(Context context) {
+        final TelephonyManager mTelephony = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        String iccid = mTelephony.getSimSerialNumber();
+        return iccid;
+    }
+
+    /**
+     * Get the Media Access Control address (MAC).
+     *
+     * @param context The context of the main Activity.
+     * @return
+     */
+    public String getMac(Context context) {
+        final WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        final WifiInfo wInfo = wifiManager.getConnectionInfo();
+        String mac = wInfo.getMacAddress();
+        return mac;
+    }
 
 }
